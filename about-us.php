2c7829d77fc16592059902cd53c24e99ff0ad351
Alexander Chai <42663780+Calexnim@users.noreply.github.com>
2022-02-07 16:47:47 +0800

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,27 @@
+<?php include 'header.php' ?>
+
+<div>
+    <div class="card">
+    <div class="container">
+        <h4><b>Alexander Chai</b></h4> 
+        <p>Websites</p> 
+    </div>
+    </div>
+    <div class="card">
+    <div class="container">
+        <h4><b>Samuel Yong</b></h4> 
+        <p>Websites</p> 
+    </div>
+    </div>
+
+    <div class="card">
+    <div class="container">
+        <h4><b>Adelina Lee</b></h4> 
+        <p>Tableau</p> 
+    </div>
+    </div>
+</div>
+
+<?php include 'footer.php' ?>
+</body>
+</html>
